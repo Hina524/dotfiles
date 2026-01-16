5fdef1776942eedc92e9e489ab4a53f0ee2eace5
Hina524 <132186412+Hina524@users.noreply.github.com>
2026-01-16 23:08:05 +0900

    programs.git.*をprograms.git.settings.*に移行、jdk23→jdk21

--- home/modules/development/git.nix
+++ home/modules/development/git.nix
@@ -3,22 +3,24 @@
   programs.git = {
     enable = true;
 
-    userName = "Hina524";
-    userEmail = "132186412+Hina524@users.noreply.github.com";
+    settings = {
+      user = {
+        name = "Hina524";
+        email = "132186412+Hina524@users.noreply.github.com";
+      };
 
-    aliases = {
-      soft = "reset --soft";
-      hard = "reset --hard";
-      s1ft = "soft HEAD~1";
-      h1rd = "hard HEAD~1";
+      alias = {
+        soft = "reset --soft";
+        hard = "reset --hard";
+        s1ft = "soft HEAD~1";
+        h1rd = "hard HEAD~1";
 
-      lg = "log --color --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit";
-      plog = "log --graph --pretty='format:%C(red)%d%C(reset) %C(yellow)%h%C(reset) %ar %C(green)%aN%C(reset) %s'";
-      tlog = "log --stat --since='1 Day Ago' --graph --pretty=oneline --abbrev-commit --date=relative";
-      rank = "shortlog -sn --no-merges";
-    };
+        lg = "log --color --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit";
+        plog = "log --graph --pretty='format:%C(red)%d%C(reset) %C(yellow)%h%C(reset) %ar %C(green)%aN%C(reset) %s'";
+        tlog = "log --stat --since='1 Day Ago' --graph --pretty=oneline --abbrev-commit --date=relative";
+        rank = "shortlog -sn --no-merges";
+      };
 
-    extraConfig = {
       init.defaultBranch = "main";
       core.editor = "code --wait";
     };
