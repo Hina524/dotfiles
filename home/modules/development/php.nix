29833aaddefae7840f4de80e45116eb0a80044fd
Hina524 <132186412+Hina524@users.noreply.github.com>
2026-01-17 11:15:56 +0900

    php.nix: 重複する拡張機能を削除
    デフォルトで有効な拡張機能(pdo, xml, curl等)を除外

--- home/modules/development/php.nix
+++ home/modules/development/php.nix
@@ -4,15 +4,9 @@ let
     { enabled, all }:
     enabled
     ++ (with all; [
-      pdo
       pdo_pgsql
       pdo_mysql
-      pdo_sqlite
       redis
-      mbstring
-      opcache
-      curl
-      xml
     ])
   );
 in
