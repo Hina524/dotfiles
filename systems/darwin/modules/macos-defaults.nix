fbd13da29a9dc8ab1bab9dc87db1b5aff7ecb10c
Hina524 <132186412+Hina524@users.noreply.github.com>
2026-02-03 17:01:33 +0900

    feat: 電源管理の設定を追加

--- systems/darwin/modules/macos-defaults.nix
+++ systems/darwin/modules/macos-defaults.nix
@@ -3,4 +3,11 @@
   system.defaults = {
     NSGlobalDomain.AppleInterfaceStyle = "Dark";
   };
+
+  system.activationScripts.postActivation.text = ''
+    # 充電中はディスプレイスリープしない
+    pmset -c displaysleep 0
+    # バッテリー時は10分でスリープ
+    pmset -b displaysleep 10
+  '';
 }
